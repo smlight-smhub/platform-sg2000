7d3fe2f817ff1a9599476f042b5971ee0924eba0
Tim Lunn <tl@smlight.tech>
2026-07-22 13:47:06 +1000

    builder: strip -pthread from linker flags to fix upstream esphome compatibility

--- builder/frameworks/freertos.py
+++ builder/frameworks/freertos.py
@@ -10,4 +10,11 @@ platform = env.PioPlatform()
 FRAMEWORK_DIR = platform.get_package_dir("framework-sg2000-rtos")
 assert FRAMEWORK_DIR
 
+# ESPHome may pull googletest which forcibly injects -pthread into global LINKFLAGS
+# on Linux. Our bare-metal toolchain riscv-none-elf-g++ compiler does not support threads
+if "-pthread" in env.get("LINKFLAGS", []):
+    env["LINKFLAGS"].remove("-pthread")
+if "-pthread" in env.get("CCFLAGS", []):
+    env["CCFLAGS"].remove("-pthread")
+
 env.SConscript(join(FRAMEWORK_DIR, "tools", "platformio-build.py"), exports="env")
